7ba36b88820633f94f710d063f4f79a6e6f291d3
Christian Finel <macreiben@gmail.com>
2024-01-31 16:16:20 +0100

    controller and view added.

---
+++ BestestTheaters/BestestTheater.WebApp/Controllers/MyBookingsControllers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BestestTheater.WebApp.Controllers
+{
+    public class MyBookingsControllers : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BestestTheaters/BestestTheater.WebApp/Pages/MyBookings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BestestTheater.WebApp.Pages
+{
+    public class MyBookingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
