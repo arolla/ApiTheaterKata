846d4fd42e04c38477cda1fb52d611ce328941d9
mathieu <mathieu.pauly@gmail.com>
2024-04-04 00:38:07 +0200

    chore(importer): generate a CSV file to import bookings

--- java/theater-importer-cli/src/main/java/org/example/Booking.java
+++ java/theater-importer-cli/src/main/java/org/example/Booking.java
@@ -1,4 +1,7 @@
 package org.example;
 
-public record Booking() {
+public record Booking(int newBookingId, int showId, int numberOfTickets, String status) {
+    public String toCsv() {
+        return String.format("%d;%d;%d;%s", newBookingId, showId, numberOfTickets, status);
+    }
 }
--- java/theater-importer-cli/src/main/java/org/example/MassBookingImportCommand.java
+++ java/theater-importer-cli/src/main/java/org/example/MassBookingImportCommand.java
@@ -8,13 +8,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @Command(group = "bookings")
 public class MassBookingImportCommand {
 
     @Command(command = "import-bookings")
-    public void massImport(String bookingFile) throws IOException {
+    public void massImport(String bookingFile, int startBookingId) throws IOException {
         Path showsPath = Paths.get("../theater-web-app/shows.csv");
         Map<Integer, Show> index = Files.readAllLines(showsPath).stream()
                 .map(line -> line.split(";"))
@@ -25,11 +26,28 @@ public class MassBookingImportCommand {
                         Integer.parseInt(parts[3])))
                 .collect(Collectors.toMap(Show::showID, show -> show));
         System.out.println("Loaded " + index.size() + " shows");
+
+        Path bookingPath = Paths.get(bookingFile);
+        AtomicInteger bookingId = new AtomicInteger(startBookingId);
+        Files.readAllLines(bookingPath).stream()
+                .map(line -> line.split(";"))
+                .map(columns -> new BookingRequest(
+                        Integer.parseInt(columns[0]),
+                        Integer.parseInt(columns[1])))
+                .forEach(request -> {
+                    Booking booking = doBooking(request, index, bookingId.getAndIncrement());
+                    System.out.println(booking.toCsv());
+                });
     }
 
-    private Booking doBooking(BookingRequest request, Map<Integer, Show> index) {
+    private Booking doBooking(BookingRequest request, Map<Integer, Show> index, int newBookingId) {
         Show show = index.get(request.showId());
-
-        return new Booking();
+        if (show == null) {
+            throw new IllegalArgumentException(String.format("Show with ID %d not found", request.showId()));
+        }
+        if (request.numberOfTickets() > show.capacity()) {
+            return new Booking(newBookingId, show.showID(), request.numberOfTickets(), "CANCELLED");
+        }
+        return new Booking(newBookingId, show.showID(), request.numberOfTickets(), "BOOKED");
     }
 }
