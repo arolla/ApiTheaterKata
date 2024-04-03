a83ec8e83fd21a353e3aa4030d94780b288517e8
mathieu <mathieu.pauly@gmail.com>
2024-04-04 00:59:39 +0200

    refactor: convert ShowsDto to a class

--- java/theater-web-app/src/main/java/com/example/bestesttheaters/api/ShowsDto.java
+++ java/theater-web-app/src/main/java/com/example/bestesttheaters/api/ShowsDto.java
@@ -1,6 +1,41 @@
 package com.example.bestesttheaters.api;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record ShowsDto(List<ShowDto> shows) {
+import java.util.List;
+import java.util.Objects;
+
+public final class ShowsDto {
+    private final List<ShowDto> shows;
+
+	@JsonCreator
+    public ShowsDto(List<ShowDto> shows) {
+        this.shows = shows;
+    }
+
+	@JsonProperty("shows")
+    public List<ShowDto> shows() {
+        return shows;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ShowsDto) obj;
+        return Objects.equals(this.shows, that.shows);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(shows);
+    }
+
+    @Override
+    public String toString() {
+        return "ShowsDto[" +
+               "shows=" + shows + ']';
+    }
+
 }
