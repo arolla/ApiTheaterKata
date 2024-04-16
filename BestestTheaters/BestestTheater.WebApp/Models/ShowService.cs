fec4740cf257a34368fd0bdce7ee83238790b685
Dorra Bartaguiz <dorra.bartaguiz@outlook.com>
2024-04-16 18:05:32 +0200

    use ShowService in MyBookings

--- BestestTheaters/BestestTheater.WebApp/Models/ShowService.cs
+++ BestestTheaters/BestestTheater.WebApp/Models/ShowService.cs
@@ -19,5 +19,38 @@
             };
             return shows;
         }
+
+        public List<BkngData_3> GetMyBookings()
+        {
+            var data = new List<BkngData_3>();
+
+            for (var i = 0; i < 10; i++)
+            {
+                var randomSeatSeed = new Random().Next(0, 100);
+
+                var randSeatCount = new Random().Next(1, 5);
+
+                var seatsAsStrings = new List<string>();
+                for (var j = 0; j < randSeatCount; j++)
+                {
+                    int randomizedNumber = new Random().Next(randomSeatSeed, randomSeatSeed + 100);
+
+                    seatsAsStrings.Add($"Seats {randomizedNumber.ToString("D2")}");
+                }
+
+                var details = seatsAsStrings.Aggregate((prev, next) => prev + ", " + next);
+
+                var oneRow = new BkngData_3()
+                {
+                    Title = $"Title{new Random().Next(0, 100).ToString("D2")}",
+                    Date = new DateTime(2024, 1, 2, 13, 16, 0, DateTimeKind.Utc),
+                    Details = details
+                };
+
+                data.Add(oneRow);
+            }
+
+            return data;
+        }
     }
 }
--- BestestTheaters/BestestTheater.WebApp/Pages/MyBookings.cshtml.cs
+++ BestestTheaters/BestestTheater.WebApp/Pages/MyBookings.cshtml.cs
@@ -1,51 +1,21 @@
-using System.Collections;
 using BestestTheater.WebApp.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BestestTheater.WebApp.Pages
 {
     public class MyBookingsModel : PageModel
     {
+        private readonly ShowService _showService;
+
+        public MyBookingsModel(ShowService showService)
+        {
+            _showService = showService;
+        }
         public IEnumerable<BkngData_3> BkngData { get; private set; }
      
         public void OnGet()
         {
-            var data = GetMyBookings();
-            BkngData = data;
-        }
-
-        private static List<BkngData_3> GetMyBookings()
-        {
-            var data = new List<BkngData_3>();
-
-            for (var i = 0; i < 10; i++)
-            {
-                var randomSeatSeed = new Random().Next(0, 100);
-
-                var randSeatCount = new Random().Next(1, 5);
-
-                var seatsAsStrings = new List<string>();
-                for (var j = 0; j < randSeatCount; j++)
-                {
-                    int randomizedNumber = new Random().Next(randomSeatSeed, randomSeatSeed + 100);
-
-                    seatsAsStrings.Add($"Seats {randomizedNumber.ToString("D2")}");
-                }
-
-                var details = seatsAsStrings.Aggregate((prev, next) => prev + ", " + next);
-
-                var oneRow = new BkngData_3()
-                {
-                    Title = $"Title{new Random().Next(0, 100).ToString("D2")}",
-                    Date = new DateTime(2024, 1, 2, 13, 16, 0, DateTimeKind.Utc),
-                    Details = details
-                };
-
-                data.Add(oneRow);
-            }
-
-            return data;
+            BkngData = _showService.GetMyBookings();
         }
     }
 }
