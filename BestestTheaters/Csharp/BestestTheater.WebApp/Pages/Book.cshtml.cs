34a1ad60dadea4e0faaa23d93475c75b8a679db8
Dorra Bartaguiz <dorra.bartaguiz@outlook.com>
2024-04-16 18:55:49 +0200

    Add Book root

---
+++ BestestTheaters/Csharp/BestestTheater.WebApp/Pages/Book.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using BestestTheater.WebApp.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BestestTheater.WebApp.Pages
+{
+    public class BookModel : PageModel
+    {
+        private readonly ILogger<BookModel> _logger;
+        private readonly ShowService _showService;
+        public string Message { get; private set; }
+
+        public BookModel(ILogger<BookModel> logger, ShowService showService)
+        {
+            _logger = logger;
+            _showService = showService;
+        }
+
+        public void OnGet()
+        {
+            var session = HttpContext.Request.Query["sessionId"].First();
+            var sessionId = int.Parse(session);
+            var shows = _showService.FetchShows();
+            var showBooked = shows.FirstOrDefault(show => show.Id == sessionId);
+            if (showBooked == null)
+            {
+                Message = "The session you are trying to book does not exist!";
+                return;
+            }
+            Message = $"You booked the session - {showBooked.Title} - of - {showBooked.Date.ToString("yyyy-MM-dd")} -!";
+        }
+    }
+}
